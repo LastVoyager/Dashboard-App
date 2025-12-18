585ac53bd5c5fc9b4f541fbcd54e3d65b96c5fc6
LastVoyager <maximvb89@gmail.com>
2025-12-19 01:40:10 +0200

    Installed default  MUI  Drawer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
-'use client'
+"use client";
 
 import Dashboard from "./dashboard/page"; // import education purposes approach
-import SideMenu from "@/components/SideMenu";
+import SideMenu from "@/components/sideMenu/SideMenu";
 import Login from "./login/page";
 import { useSession } from "next-auth/react";
 
-function Home () {
+function Home() {
   const { data: session } = useSession();
-  
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
+    <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
       {session && (
         <>
           <SideMenu />
-          <Dashboard /> 
-        </>)}
+          <Dashboard />
+        </>
+      )}
       <Login />
     </div>
   );
